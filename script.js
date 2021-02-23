9dc6bb8d07ff7756c0a10a8505926b1c82e98dfc
Benjamin-del <67153251+Benjamin-del@users.noreply.github.com>
2021-02-23 08:20:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/satellite-streets-v11",
   center: [-76.469368, 44.77903],
-  zoom: 6,
+  zoom: 7.61,
   hash:true
 });
 
